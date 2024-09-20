bfdb3565d61bcf08cbdc4c5066a658202466fec2
Saahil Kataria <saahil.kataria@gmail.com>
2024-09-21 02:10:08 +0530

    changing tests1

--- tests/test_model_performance.py
+++ tests/test_model_performance.py
@@ -1,8 +1,30 @@
 import tensorflow as tf
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+from model_utils import create_model
+from config import Config
+import pytest
+# Add the project root directory to the system path
+
+@pytest.fixture
+def config():
+    return Config()
+
 
 def test_model_accuracy():
     # Load the best model and validate its performance
+    paths = config.get_paths()
+    dataset_params = config.get_dataset_params()
+    
     model = tf.keras.models.load_model("models/best_model_VGG16.keras")
+
+    base_model = tf.keras.applications.VGG16(weights=None, include_top=False, input_shape=(256, 256, 3))
+    model = create_model(base_model, num_classes=len(dataset_params['class_names']))
+
+    # Load the checkpoint weights
+    model.load_weights("models/checkpoint_model.h5")
+
 
     # Simulate or use actual validation data
     val_dataset = tf.keras.utils.image_dataset_from_directory(
